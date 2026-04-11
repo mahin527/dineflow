20c309ef1c521588063f3a70ac33444e6ae8ebbe
mahin527 <hasan.mahin527@gmail.com>
2026-04-11 11:06:59 +0600

    feat: loading component updated

--- Client/src/components/Loading.tsx
+++ Client/src/components/Loading.tsx
@@ -2,7 +2,7 @@ import { Loader } from "lucide-react";
 
 const Loading = () => {
     return (
-        <div className="min-h-screen bg-linear-to-br from-neutral-300 via-bg-lightGreen to-neutral-900 flex items-center justify-center relative overflow-hidden">
+        <div className="min-h-screen bg-linear-to-br from-neutral-300 via-bg-lightGreen to-neutral-900 flex flex-col items-center justify-center relative overflow-hidden">
             <Loader className="animate-spin w-16 h-16 text-white" />
             <h2 className="text-xl md:text-2xl xl:text-3xl pt-3 font-bold">
                 Waking up the server... Hang tight, good food is on the way!
