8d0c5bcf0952dff1f80a930c6f834ea5f1d243e8
mahin527 <hasan.mahin527@gmail.com>
2026-04-11 11:06:02 +0600

    feat: loading component updated

--- Client/src/components/Loading.tsx
+++ Client/src/components/Loading.tsx
@@ -4,6 +4,9 @@ const Loading = () => {
     return (
         <div className="min-h-screen bg-linear-to-br from-neutral-300 via-bg-lightGreen to-neutral-900 flex items-center justify-center relative overflow-hidden">
             <Loader className="animate-spin w-16 h-16 text-white" />
+            <h2 className="text-xl md:text-2xl xl:text-3xl pt-3 font-bold">
+                Waking up the server... Hang tight, good food is on the way!
+            </h2>
         </div>
     );
 };
